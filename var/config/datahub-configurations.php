e3a5dd8a9f98f33d4c875547faa64675b62d4232
rajesh <developerrajeshgupta@gmail.com>
2023-08-06 19:35:17 +0530

    add another channels

--- var/config/datahub-configurations.php
+++ var/config/datahub-configurations.php
@@ -10,6 +10,7 @@ return [
                 "active" => TRUE,
                 "type" => "dataImporterDataObject",
                 "name" => "Jp_channel1",
+                "group" => "jp",
                 "description" => "",
                 "path" => NULL
             ],
@@ -67,6 +68,7 @@ return [
                 "type" => "dataImporterDataObject",
                 "name" => "JP_channel2",
                 "description" => "",
+                "group" => "jp",
                 "path" => NULL
             ],
             "loaderConfig" => [
@@ -123,6 +125,7 @@ return [
                 "type" => "dataImporterDataObject",
                 "name" => "JP_channel3",
                 "description" => "",
+                "group" => "jp",
                 "path" => NULL
             ],
             "loaderConfig" => [
@@ -168,6 +171,72 @@ return [
             ],
             "executionConfig" => [
                 "cronDefinition" => ""
+            ],
+            "workspaces" => [
+
+            ]
+        ],
+        "JP_about" => [
+            "general" => [
+                "active" => TRUE,
+                "type" => "dataImporterDataObject",
+                "name" => "JP_about",
+                "description" => "",
+                "path" => NULL
+            ],
+            "loaderConfig" => [
+                "type" => "upload",
+                "settings" => [
+                    "uploadFilePath" => "JP_about/upload.import"
+                ]
+            ],
+            "interpreterConfig" => [
+                "type" => "csv",
+                "settings" => [
+                    "skipFirstRow" => TRUE,
+                    "delimiter" => ",",
+                    "enclosure" => "\"",
+                    "escape" => "\\"
+                ]
+            ],
+            "resolverConfig" => [
+                "elementType" => "dataObject",
+                "dataObjectClassId" => "1",
+                "loadingStrategy" => [
+                    "type" => "notLoad"
+                ],
+                "createLocationStrategy" => [
+                    "type" => "staticPath",
+                    "settings" => [
+                        "path" => "/"
+                    ]
+                ],
+                "locationUpdateStrategy" => [
+                    "type" => "noChange"
+                ],
+                "publishingStrategy" => [
+                    "type" => "noChangeUnpublishNew"
+                ]
+            ],
+            "processingConfig" => [
+                "executionType" => "parallel",
+                "idDataIndex" => ""
+            ],
+            "mappingConfig" => [
+
+            ],
+            "executionConfig" => [
+                "cronDefinition" => ""
+            ],
+            "workspaces" => [
+
+            ]
+        ],
+        "test" => [
+            "general" => [
+                "type" => "dataImporterDataObject",
+                "path" => NULL,
+                "name" => "test"
             ],
             "workspaces" => [
 
